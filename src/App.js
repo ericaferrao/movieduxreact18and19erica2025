14037bcfc49ad7a487b60304c2033d2cd22e6750
Erica Stefny Ferrao <ferraoerica@gmail.com>
2025-11-08 09:27:57 -0800

    useState useEffect simple usag in Movies Grid component (#7)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import './styles.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className='container'>
         <Header />
       </div>
+      <MoviesGrid />
       <Footer />
     </div>
   );
