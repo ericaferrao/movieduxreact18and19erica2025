61c7699e557dc498c6e0c18c974416cb0bf1fd60
Erica Stefny Ferrao <ferraoerica@gmail.com>
2025-11-09 05:46:22 -0800

    on Error error handling for bad image (#11)

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,10 +2,14 @@ import '../styles.css'
 
 export default function MovieCard({ movie }) {
 
+    const handleError = (e) => {
+        e.target.src = 'images/default.jpg'
+    }
+
     return (
         <>
             <div key={movie.id} className="movie-card">
-                <img src={`images/${movie.image}`} alt={movie.title} />
+                <img src={`images/${movie.image}`} alt={movie.title} onError={handleError} />
                 <div className="movie-card-info">
                     <div className="movie-card-title">{movie.title}</div>
                     <div className="movie-card-genre">{movie.genre}</div>
