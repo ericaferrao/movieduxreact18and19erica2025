f7ecd391cfb8569479416d42ece228828833c57c
Erica Stefny Ferrao <ferraoerica@gmail.com>
2025-11-10 07:10:26 -0800

    configured the router and its routes intalled the rreact-router-dom library

--- src/App.js
+++ src/App.js
@@ -2,15 +2,36 @@ import './App.css';
 import './styles.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import MoviesGrid from './components/MoviesGrid'
+import MoviesGrid from './components/MoviesGrid';
+import WatchList from './components/WatchList';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
         <Header />
+
+
+
       </div>
-      <MoviesGrid />
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/watchlist">WatchList</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<MoviesGrid />}></Route>
+          <Route path="/watchlist" element={<WatchList />}></Route>
+        </Routes>
+
+      </Router>
       <Footer />
     </div>
   );
---
+++ src/components/WatchList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles.css'
+
+export default function WatchList() {
+    return (
+        <>
+            <div >
+                <h1 className="title"> WatchList</h1>
+            </div>
+        </>
+    )
+}
